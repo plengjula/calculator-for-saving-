1d05f1ef25d66b86a6904bd8271e82d08f15f46d
plengjulalak <en_jula@hotmail.com>
2020-01-14 13:24:38 +0900

    First update
    first commit

---
+++ calculate money/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculate_money
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtCal_Click(object sender, RoutedEventArgs e)
+        {
+            int income = int.Parse(txtIncome.Text);
+            int expense = int.Parse(txtExpense.Text);
+            int want = int.Parse(txtWant.Text);
+            int result = (want / (income - expense));
+
+            txtday.Text = result.ToString();
+
+        }
+    }
+}
